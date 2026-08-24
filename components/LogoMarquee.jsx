7fe4807db9dd21661abbe9b68cc95abc78db7b52
Imama Ansari <ansariimama@yahoo.com>
2026-08-25 00:11:51 +0500

    Add animation in Logo in About Section

--- components/LogoMarquee.jsx
+++ components/LogoMarquee.jsx
@@ -2,11 +2,9 @@ const brands = [
   { name: "Amazon", img: "/BrandLogos/Amazon.jpg" },
   { name: "Walmart", img: "/BrandLogos/Walmart.png" },
   { name: "Lowe's", img: "/BrandLogos/lowes.png" },
-  { name: "Target", img: "/BrandLogos/target.png" },
+  { name: "Target", img: "/BrandLogos/target.jpg" },
   { name: "Wayfair", img: "/BrandLogos/Wayfair.jpg" },
-  { name: "Home Depot", img: "/BrandLogos/home-depot.png" },
-  { name: "Best Buy", img: "/brands/best-buy.png" },
-  { name: "Dollar General", img: "/brands/dollar-general.png" },
+  { name: "Home Depot", img: "/BrandLogos/home-depot1.jpg" },
 ];
 
 export default function LogoMarquee() {
@@ -14,8 +12,10 @@ export default function LogoMarquee() {
     <div
       className="relative w-full overflow-hidden"
       style={{
-        maskImage: "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
-        WebkitMaskImage: "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+        maskImage:
+          "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+        WebkitMaskImage:
+          "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
       }}
     >
       <div className="flex w-max gap-10 animate-marquee hover:[animation-play-state:paused]">
@@ -24,7 +24,11 @@ export default function LogoMarquee() {
             key={`${b.name}-${i}`}
             className="flex items-center justify-center h-20 w-40 shrink-0 bg-white rounded-xl shadow-card px-6"
           >
-            <img src={b.img} alt={b.name} className="max-h-9 w-auto object-contain" />
+            <img
+              src={b.img}
+              alt={b.name}
+              className="max-h-20 w-auto object-contain"
+            />
           </div>
         ))}
       </div>
